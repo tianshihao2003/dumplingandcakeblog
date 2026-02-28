3da78af881d9a81eab98a6e4729859568bd7654a
dumplingandcake <3109581507@qq.com>
2026-02-28 19:06:14 +0800

    添加网站页面

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -44,6 +44,15 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 	// 友链
 	links.push(LinkPreset.Friends);
 
+	
+	// 项目推荐
+	links.push({
+		name: "常用网站",
+		url: "/projects/",
+		icon: "material-symbols:star",
+	});
+
+
 	// 根据配置决定是否添加留言板，在siteConfig关闭pages.guestbook时导航栏不显示留言板
 	if (siteConfig.pages.guestbook) {
 		links.push(LinkPreset.Guestbook);
---
+++ src/config/projectsConfig.ts
@@ -0,0 +1,447 @@
+import type { FriendLink } from "../types/config";
+
+export const projectsPageConfig = {
+	columns: 3,
+};
+
+export const projectsConfig: FriendLink[] = 
+[
+  {
+    "title": "知乎",
+    "imgurl": "https://www.zhihu.com/favicon.ico",
+    "desc": "中文互联网高质量问答社区",
+    "siteurl": "https://www.zhihu.com/",
+    "tags": ["社区网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "稀土掘金",
+    "imgurl": "https://juejin.cn/favicon.ico",
+    "desc": "前端开发者技术社区",
+    "siteurl": "https://juejin.cn/",
+    "tags": ["社区网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "编程教程",
+    "imgurl": "https://www.codefather.cn/favicon.ico",
+    "desc": "实用编程学习教程资源",
+    "siteurl": "https://www.codefather.cn/course",
+    "tags": ["社区网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "思源笔记 - 链滴",
+    "imgurl": "https://ld246.com/favicon.ico",
+    "desc": "基于双向链接的知识管理平台",
+    "siteurl": "https://ld246.com/tag/siyuan",
+    "tags": ["社区网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "微博",
+    "imgurl": "https://weibo.com/favicon.ico",
+    "desc": "中国主流社交媒体平台",
+    "siteurl": "https://weibo.com/",
+    "tags": ["社区网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "博客首页",
+    "imgurl": "https://bk.tsh520.cn/favicon.ico",
+    "desc": "个人技术博客主页",
+    "siteurl": "https://bk.tsh520.cn/",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "博客仪表盘",
+    "imgurl": "https://bk.tsh520.cn/favicon.ico",
+    "desc": "WordPress后台管理界面",
+    "siteurl": "https://bk.tsh520.cn/wp-admin/",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "团子的邮箱",
+    "imgurl": "https://email.0824.uk/favicon.ico",
+    "desc": "个人使用的邮箱服务",
+    "siteurl": "https://email.0824.uk/inbox",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "团子的相册",
+    "imgurl": "https://ph.tsh520.cn/favicon.ico",
+    "desc": "个人照片存储与展示",
+    "siteurl": "https://ph.tsh520.cn/",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "宝塔Linux面板",
+    "imgurl": "https://www.bt.cn/favicon.ico",
+    "desc": "服务器可视化管理工具",
+    "siteurl": "https://119.45.92.169:34242/851dcbc6",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "团子和蛋糕的博客",
+    "imgurl": "https://blog.tsh520.cn/favicon.ico",
+    "desc": "个人技术与生活分享博客",
+    "siteurl": "https://blog.tsh520.cn/",
+    "tags": ["我的网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "哲风壁纸",
+    "imgurl": "https://haowallpaper.com/favicon.ico",
+    "desc": "高清壁纸下载网站",
+    "siteurl": "https://haowallpaper.com/",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Emoji词典",
+    "imgurl": "https://www.emojiall.com/favicon.ico",
+    "desc": "全面的emoji表情查询工具",
+    "siteurl": "https://www.emojiall.com/",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "樱之空动漫",
+    "imgurl": "https://skr.skr3.cc:666/favicon.ico",
+    "desc": "动漫资源分享平台",
+    "siteurl": "https://skr.skr3.cc:666/user/plays.html",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "100%多功能图片转换器",
+    "imgurl": "https://imagestool.com/favicon.ico",
+    "desc": "在线图片格式转换工具",
+    "siteurl": "https://imagestool.com/webp2jpg-online/",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Google Fonts",
+    "imgurl": "https://fonts.google.com/favicon.ico",
+    "desc": "免费开源字体库",
+    "siteurl": "https://fonts.google.com/?query=Inter",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "我的图库",
+    "imgurl": "https://picui.cn/favicon.ico",
+    "desc": "个人图片管理与存储",
+    "siteurl": "https://picui.cn/user/images",
+    "tags": ["资源网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Fomalhaut🥝",
+    "imgurl": "https://www.fomal.cc/favicon.ico",
+    "desc": "技术博主个人网站",
+    "siteurl": "https://www.fomal.cc/",
+    "tags": ["大佬博客收藏"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "海拥",
+    "imgurl": "https://blog.haiyong.site/favicon.ico",
+    "desc": "前端技术分享博客",
+    "siteurl": "https://blog.haiyong.site/",
+    "tags": ["大佬博客收藏"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "喵斯基部落",
+    "imgurl": "https://blog.moewah.com/favicon.ico",
+    "desc": "运维技术与经验分享",
+    "siteurl": "https://blog.moewah.com/",
+    "tags": ["大佬博客收藏"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Cloudflare",
+    "imgurl": "https://dash.cloudflare.com/favicon.ico",
+    "desc": "全球CDN与网络安全服务",
+    "siteurl": "https://dash.cloudflare.com/2668bf8cf06b94446afeb47b8b23496f/home/domains",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "蓝奏云",
+    "imgurl": "https://pc.woozooo.com/favicon.ico",
+    "desc": "文件分享与网盘服务",
+    "siteurl": "https://pc.woozooo.com/mydisk.php",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "学习通",
+    "imgurl": "https://i.chaoxing.com/favicon.ico",
+    "desc": "高校在线教学平台",
+    "siteurl": "https://i.chaoxing.com/base?t=1765278343620",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "樱之空动漫",
+    "imgurl": "https://skr.skr3.cc:666/favicon.ico",
+    "desc": "动漫资源分享平台",
+    "siteurl": "https://skr.skr3.cc:666/user/plays.html",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "宝塔官网",
+    "imgurl": "https://www.bt.cn/favicon.ico",
+    "desc": "服务器管理面板官方站点",
+    "siteurl": "https://www.bt.cn/login.html",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "腾讯轻量云",
+    "imgurl": "https://console.cloud.tencent.com/favicon.ico",
+    "desc": "腾讯云轻量应用服务器控制台",
+    "siteurl": "https://console.cloud.tencent.com/lighthouse/instance/detail?searchParams=rid%3D33&rid=33&id=lhins-2yxvw6sp&tab=firewall",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "又拍云",
+    "imgurl": "https://www.upyun.com/favicon.ico",
+    "desc": "云存储与CDN服务",
+    "siteurl": "https://www.upyun.com/",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "又拍云控制台",
+    "imgurl": "https://console.upyun.com/favicon.ico",
+    "desc": "又拍云服务管理后台",
+    "siteurl": "https://console.upyun.com/services/bktsh520cn/filemanage/",
+    "tags": ["常用网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "在线本地字体预览",
+    "imgurl": "https://www.lddgo.net/favicon.ico",
+    "desc": "本地字体在线预览工具",
+    "siteurl": "https://www.lddgo.net/convert/local-font-preview",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "在线ping",
+    "imgurl": "https://www.itdog.cn/favicon.ico",
+    "desc": "多线路网络延迟测试工具",
+    "siteurl": "https://www.itdog.cn/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "ChatGPT",
+    "imgurl": "https://chatgpt.com/favicon.ico",
+    "desc": "OpenAI聊天机器人",
+    "siteurl": "https://chatgpt.com/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "爱玩Co",
+    "imgurl": "http://cocfz.com/favicon.ico",
+    "desc": "部落冲突辅助工具",
+    "siteurl": "http://cocfz.com/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "免费在线抠图",
+    "imgurl": "https://www.koukoutu.com/favicon.ico",
+    "desc": "无需上传的在线图像抠图工具",
+    "siteurl": "https://www.koukoutu.com/removebgtool/all",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Free Font 艺术体",
+    "imgurl": "https://font.ittools.cc/favicon.ico",
+    "desc": "商用免费字体收集",
+    "siteurl": "https://font.ittools.cc/art",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "ZeoSeven Fonts",
+    "imgurl": "https://fonts.zeoseven.com/favicon.ico",
+    "desc": "字体浏览与下载平台",
+    "siteurl": "https://fonts.zeoseven.com/browse/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "释放字体自由",
+    "imgurl": "https://fonts.zeoseven.com/favicon.ico",
+    "desc": "字体资源分享网站",
+    "siteurl": "https://fonts.zeoseven.com/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "100%多功能图片转换器（vpn）",
+    "imgurl": "https://imagestool.com/favicon.ico",
+    "desc": "在线图片格式转换工具",
+    "siteurl": "https://imagestool.com/webp2jpg-online/",
+    "tags": ["工具网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "学习通",
+    "imgurl": "https://i.chaoxing.com/favicon.ico",
+    "desc": "高校在线教学平台",
+    "siteurl": "https://i.chaoxing.com/base?t=1766907388357",
+    "tags": ["学校网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "深澜软件",
+    "imgurl": "http://218.198.32.106/favicon.ico",
+    "desc": "校园网认证登录系统",
+    "siteurl": "http://218.198.32.106/srun_portal_pc?ac_id=1&theme=pro",
+    "tags": ["学校网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Maps | MapTiler Cloud",
+    "imgurl": "https://cloud.maptiler.com/favicon.ico",
+    "desc": "地图服务与可视化平台",
+    "siteurl": "https://cloud.maptiler.com/maps/",
+    "tags": ["其他网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Qwerty Learner",
+    "imgurl": "https://qwerty.kaiyi.cool/favicon.ico",
+    "desc": "键盘工作者单词记忆软件",
+    "siteurl": "https://qwerty.kaiyi.cool/gallery",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "微信读书",
+    "imgurl": "https://weread.qq.com/favicon.ico",
+    "desc": "微信推出的电子阅读平台",
+    "siteurl": "https://weread.qq.com/web/shelf",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Java 教程 | 菜鸟教程",
+    "imgurl": "https://www.runoob.com/favicon.ico",
+    "desc": "基础编程教程网站",
+    "siteurl": "https://www.runoob.com/java/java-tutorial.html",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Java教程 - 廖雪峰",
+    "imgurl": "https://liaoxuefeng.com/favicon.ico",
+    "desc": "深入浅出的Java教程",
+    "siteurl": "https://liaoxuefeng.com/books/java/introduction/index.html",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "面试鸭",
+    "imgurl": "https://www.mianshiya.com/favicon.ico",
+    "desc": "程序员面试题库平台",
+    "siteurl": "https://www.mianshiya.com/",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "力扣 (LeetCode)",
+    "imgurl": "https://leetcode.cn/favicon.ico",
+    "desc": "算法与数据结构练习平台",
+    "siteurl": "https://leetcode.cn/",
+    "tags": ["学习网站"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "豆包",
+    "imgurl": "https://www.doubao.com/favicon.ico",
+    "desc": "字节跳动推出的AI助手",
+    "siteurl": "https://www.doubao.com/chat/?channel=baidu_sem&source=dbweb_sem_pinp_hexin_ocpc_faxx_21&ad_platform_id=baidusearch_lead",
+    "tags": ["AI工具"],
+    "weight": 8,
+    "enabled": true
+  },
+  {
+    "title": "Firefly Docs",
+    "imgurl": "https://docs-firefly.cuteleaf.cn/favicon.ico",
+    "desc": "Firefly项目文档",
+    "siteurl": "https://docs-firefly.cuteleaf.cn/zh/",
+    "tags": ["文档"],
+    "weight": 8,
+    "enabled": true
+  },
+];
+
+export const getEnabledProjects = (): FriendLink[] => {
+	return projectsConfig
+		.filter((project) => project.enabled)
+		.sort((a, b) => b.weight - a.weight);
+};
